86caaa0bdd309b87a35954ecda15ab65b3e53f02
obayedsiam <obayedsiam@gmail.com>
2022-05-16 19:06:03 +0600

    up

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# This is a sample Python script.
+# This is a sample Python script from https://www.tensorflow.org/tutorials/keras/text_classification
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
@@ -24,8 +24,20 @@ if __name__ == '__main__':
     print_hi('PyCharm')
     print(tf.__version__)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    url = "https://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz"
 
+    dataset = tf.keras.utils.get_file("aclImdb_v1", url,
+                                      untar=True, cache_dir='.',
+                                      cache_subdir='')
 
+    dataset_dir = os.path.join(os.path.dirname(dataset), 'aclImdb')
+    train_dir = os.path.join(dataset_dir, 'train')
+    os.listdir(train_dir)
 
+    sample_file = os.path.join(train_dir, 'pos/1181_9.txt')
+    with open(sample_file) as f:
+        print(f.read())
+
+        remove_dir = os.path.join(train_dir, 'unsup')
+        shutil.rmtree(remove_dir)
 
